7f13082160d68fe101ec971833639cd841827c80
Alvaro <alvarocaber@gmail.com>
2024-07-02 12:35:33 +0400

    Changes in test

--- test.py
+++ test.py
@@ -1,4 +1,16 @@
+"""" This is a test file to test the functionality of the test function."""
+
 import pandas as pd
 
 # TODO #1 create issue for this @alvaro-cabrera
 # Solved by @alvaro-cabrera
+
+
+def test() -> str:
+    """This is a test function that prints 'Hello World'."""   
+    return print("Hello World")
+
+
+if __name__ == "__main__":
+    test()
+    # EOF
